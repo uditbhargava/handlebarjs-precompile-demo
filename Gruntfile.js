18025c838abcfde8b6f4f0248b2e19ad4e514db7
sumanbogati <sumanbogati@gmail.com>
2017-05-19 14:50:51 +0530

    add comment in grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,16 +2,22 @@ module.exports = function(grunt) {
     grunt.initConfig({
         handlebars: {
             all: {
+				
                 files: {
-                    //"js/templates.js": ["templates/**/*.hbs"]
-                    "js/all.js": ["templates/**/*.hbs", "templates/*.hbs"]
+                    
+                    // converting all .hbs(handlerbar template file ) into all.js file
+                    // you can change the directory according to your requirement
+                    "js/all.js": ["templates/*.hbs"]
                 }
             }
         },
 
         watch: {
+			/** the grunt is waching if any changes in .hbs files 
+			 *  if so there will be peform the task hanldebars which does precompile (.hbs to .js)
+			 *  **/
             templates : {
-                files: ["templates/**/*.hbs", "templates/*.hbs"],
+			    files: ["templates/*.hbs"],
                 tasks: ['handlebars']
             },
 
@@ -22,5 +28,4 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-handlebars');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.registerTask('default', ['handlebars']);
-
 };

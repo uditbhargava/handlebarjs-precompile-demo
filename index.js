8f39fb3e70bc45424d9029fdc10044bc1376c448
sumanbogati <sumanbogati@gmail.com>
2017-05-28 14:49:18 +0530

    add partial example

--- index.js
+++ index.js
@@ -9,27 +9,17 @@ window.onload = function (){
 
 	 //var html = templateScript(context);
 	//html = 'My name is Ritesh Kumar . I am a developer.'
-	var template = JST['templates/ppt.hbs'];
+	var template = JST['templates/leftbar.hbs'];
 	var html = template(context);
-	$(document.body).append(html);
-
-    var context2 = { "quiz" : "physics"};
-    var template2 = JST['templates/quiz.hbs'];
-    var html2 = template2(context2);
-    $(document.body).append(html2);
-
-
-    var context3 = { "home" : "main home"};
-    var template3 = JST['templates/poll/home.hbs'];
-    var html3 = template3(context3);
-    $(document.body).append(html3);
-
-
-
-    var context4 = { "report" : "student poll"};
-    var template4 = JST['templates/poll/report.hbs'];
-    var html4 = template4(context4);
-    $(document.body).append(html4);
-
+    Handlebars.registerPartial("leftbar", html);
+    
+    var template = JST['templates/right.hbs'];
+	var html2 = template(context);
+    Handlebars.registerPartial("rightbar", html2);
+    
+    var template = JST['templates/main.hbs'];
+	var html3 = template();
+    
+	$(document.body).append(html3);
 }
 
---
+++ js/all.js
@@ -0,0 +1,26 @@
+this["JST"] = this["JST"] || {};
+
+this["JST"]["templates/leftbar.hbs"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var helper;
+
+  return "<div>\n	I am "
+    + container.escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"name","hash":{},"data":data}) : helper)))
+    + " and I am in leftbar.	\n</div>\n";
+},"useData":true});
+
+this["JST"]["templates/main.hbs"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return "<div id=\"mainContainr\">\n"
+    + ((stack1 = container.invokePartial(partials.leftbar,depth0,{"name":"leftbar","data":data,"indent":"    ","helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "")
+    + ((stack1 = container.invokePartial(partials.rightbar,depth0,{"name":"rightbar","data":data,"indent":"    ","helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "")
+    + "</div>\n";
+},"usePartial":true,"useData":true});
+
+this["JST"]["templates/right.hbs"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var helper;
+
+  return "<div>\n    I am "
+    + container.escapeExpression(((helper = (helper = helpers.occupation || (depth0 != null ? depth0.occupation : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"occupation","hash":{},"data":data}) : helper)))
+    + " and I am in rightbar.\n</div>\n";
+},"useData":true});
